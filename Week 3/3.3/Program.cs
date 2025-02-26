759e5d3f7a73429a314a575afddcd7a47f13ecd3
Cultz20 <cultzbtra@gmail.com>
2025-02-26 10:25:06 +0700

    Add files via upload

---
+++ Week 3/3.3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Kiet;
+using SplashKitSDK;
+
+namespace Kiet
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Window wn = new Window("Shape Drawer", 800, 600);
+            Drawing myDrawing = new Drawing();
+            do
+            {
+                SplashKit.ProcessEvents();
+                SplashKit.ClearScreen();
+
+                if(SplashKit.MouseClicked(MouseButton.LeftButton))
+                {
+                    Shape sp = new Shape(51);
+                    sp.X = (float)SplashKit.MousePosition().X;
+                    sp.Y = (float)SplashKit.MousePosition().Y;
+                    myDrawing.AddShape(sp);
+                }
+
+                if (SplashKit.MouseClicked(MouseButton.RightButton))
+                {
+                    Point2D pt = new Point2D();
+                    pt.X = SplashKit.MousePosition().X;
+                    pt.Y = SplashKit.MousePosition().Y;
+                    Shape selected = myDrawing.selectShapeAt(pt);
+
+                    if (selected != null)
+                    {
+                        selected.Selected = !selected.Selected; 
+                    }
+                }
+                
+                if (SplashKit.KeyTyped(KeyCode.SpaceKey))
+                {
+                    myDrawing.Background = SplashKit.RandomColor();
+                }
+
+                if (SplashKit.KeyTyped(KeyCode.BackspaceKey) || SplashKit.KeyTyped(KeyCode.DeleteKey))
+                {
+                    List<Shape> shapes = new List<Shape>();
+                    shapes = myDrawing.SelectedShape;
+                    foreach (var s in shapes)
+                        myDrawing.RemoveShape(s);
+                }
+
+                myDrawing.Draw();
+                SplashKit.RefreshScreen();
+            } while (!wn.CloseRequested);
+        }
+    }
+}
---
+++ Week 3/3.3/Shape.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SplashKitSDK;
+
+namespace Kiet
+{
+    internal class Shape
+    {
+        Color _color;
+        float _x, _y;
+        int _width, _height;
+        Boolean _selected;
+
+        public Shape(int param) { 
+            _color = Color.Azure;
+            _x = 0;
+            _y = 0;
+            _width = 100 + param;
+            _height = 100 + param;
+            _selected = false;
+        }
+
+        public Color Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public float X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public float Y
+        {
+            get { return _y; }
+            set { _y = value; }
+
+        }
+        public int Width
+        {
+            get { return _width; } 
+            set { _width = value; }
+        }
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+        public Boolean Selected
+        {
+            get { return _selected; }
+            set { _selected = value; }
+
+        }
+        public void Draw()
+        {
+            if (_selected) 
+                DrawOutline();
+            SplashKit.FillRectangle(_color, _x, _y, _width, _height);
+        }
+        public Boolean IsAt(Point2D pt)
+        {
+            if((pt.X > _x) && (pt.X < _x + _width) && (pt.Y > _y) && (pt.Y < _y + _height))
+            return true;
+            else return false;
+        }
+        public void DrawOutline()
+        {
+            Rectangle rt = new Rectangle();
+            rt.X = _x - 6;
+            rt.Y = _y - 6;
+            rt.Width = _width + 12;
+            rt.Height = _height + 12;
+            SplashKit.DrawRectangle(Color.Black, rt);
+        }
+    }
+}   
