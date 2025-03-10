d7726846150f421c9e5ad6b69fd0587fff822709
Cultz20 <cultzbtra@gmail.com>
2025-03-10 18:09:50 +0700

    Add files via upload

---
+++ Week 2/2.4 Iteration 1/IdentifiableObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+public class IdentifiableObject
+{
+    private List<string> _identifiers;
+
+    public IdentifiableObject(string[] idents) 
+    {
+        _identifiers = new List<string>(idents);
+    }
+
+    public bool AreYou(string id)
+    { 
+        return _identifiers.Contains(id); 
+    }
+
+    public string FirstID
+    { get { return _identifiers[0]; } }
+    public void AddIdentifier(string id)
+    {
+        _identifiers.Add(id.ToLower());
+    }
+
+    public void PrivilegeEscalation(string pin)
+    {
+        string studentId = "104773652";
+        string tutorialId = "COS20007"; 
+
+        if (studentId.Length >= 4 && pin == studentId.Substring(studentId.Length - 4))
+        {
+            if (_identifiers.Count > 0)
+            {
+                _identifiers[0] = tutorialId;
+            }
+        }
+    }
+}
