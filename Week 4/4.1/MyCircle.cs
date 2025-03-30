f5418534c11aa0cc33e2b1a9f887af9d42ed0f1e
Cultz20 <cultzbtra@gmail.com>
2025-03-30 09:25:34 +0700

    Add files via upload

---
+++ Week 4/4.1/MyCircle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SplashKitSDK;
+
+namespace Kiet
+{
+    internal class MyCircle : Shape
+{
+        int _radius;
+        public int Radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+        public MyCircle() : this(Color.Blue, 0.0f, 0.0f, 50 + 51) { }
+
+        public MyCircle(Color color, float x, float y, int radius) : base(color)
+        {
+            _radius = radius;
+        }
+
+        public override void DrawOutline()
+        {
+            SplashKit.DrawCircle(Color.Black, X, Y, Radius + 2);
+        }
+
+        public override void Draw()
+        {
+            if (Selected)
+            DrawOutline();
+            SplashKit.FillCircle(Color, X, Y, Radius);
+        }
+        public override Boolean IsAt(Point2D pt)
+        {
+            return SplashKit.PointInCircle(pt, SplashKit.CircleAt(X, Y, Radius));
+        }
+    }
+}
---
+++ Week 4/4.1/MyRectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SplashKitSDK;
+
+namespace Kiet
+{
+    public class MyRectangle : Shape
+    {
+        public MyRectangle() : this(Color.Green, 0.0f, 0.0f, 100 + 51, 100 + 51) { }
+
+        public MyRectangle(Color color, float x, float y, int width, int height) : base(color, x, y, width, height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+
+        }
+
+        public override void Draw()
+        {
+            if (Selected)
+                DrawOutline();
+            SplashKit.FillRectangle(Color, X, Y, Width, Height);
+        }
+
+        public override void DrawOutline()
+        {
+            SplashKit.DrawRectangle(Color.Black, X - 6, Y - 6, Width + 12, Height + 12);
+        }
+
+        public override Boolean IsAt(Point2D pt)
+        {
+            return (pt.X > X) && (pt.X < X + Width) && (pt.Y > Y) && (pt.Y < Y + Height);
+        }
+    }
+}
