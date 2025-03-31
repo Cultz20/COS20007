c8ae28849b62551ee2ee5051f0b7ad5120b1a566
Cultz20 <cultzbtra@gmail.com>
2025-03-31 09:47:53 +0700

    Add files via upload

---
+++ 4.1/MyLine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SplashKitSDK;
+
+namespace Kiet
+{
+    public class MyLine : Shape
+    {
+        public MyLine() : this(Color.Red, 0.0f, 0.0f, 50) { }
+
+        public MyLine(Color color, float x, float y, int width) : base(color, x, y, width, 2)
+        {
+            Width = width;
+        }
+
+        public override void Draw()
+        {
+            if (Selected)
+            SplashKit.DrawLine(Color, X, Y, X + Width + 20, Y);
+        }
+
+        public override void DrawOutline()
+        {
+            SplashKit.DrawLine(Color.Black, X, Y - 1, X + Width + 20, Y - 1);
+            SplashKit.DrawLine(Color.Black, X, Y + 1, X + Width + 20, Y + 1);
+        }
+
+        public override Boolean IsAt(Point2D pt)
+        {
+            float range = 2.0f;
+            return (pt.X >= X && pt.X <= X + Width + 20) && (Math.Abs(pt.Y - Y) <= range);
+        }
+    }
+}
