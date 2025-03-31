b732f17aaf292e494e61f9540c1aa8a32e4ae90e
Cultz20 <cultzbtra@gmail.com>
2025-03-31 10:00:53 +0700

    Add files via upload

---
+++ Week 4/4.2/IdentifiableObject.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations
+{
+    public class IdentifiableObject
+    {
+        protected List<string> identifiers;
+
+        protected IdentifiableObject(string[] idList)
+        {
+            identifiers = new List<string>(idList.Select(id => id.ToLower()));
+        }
+
+        public bool AreYou(string id)
+        {
+            return identifiers.Contains(id.ToLower());
+        }
+
+        public string FirstID()
+        {
+            return identifiers.Count > 0 ? identifiers[0] : "";
+        }
+
+        public void AddIdentifier(string id)
+        {
+            if (!identifiers.Contains(id.ToLower()))
+            {
+                identifiers.Add(id.ToLower());
+            }
+        }
+
+        public void ReplaceFirstID(string newID)
+        {
+            if (identifiers.Count > 0)
+            {
+                identifiers[0] = newID.ToLower();
+            }
+            else
+            {
+                identifiers.Add(newID.ToLower());
+            }
+        }
+    }
+
+}
+
