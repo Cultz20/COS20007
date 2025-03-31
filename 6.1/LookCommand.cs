c3163dfe7fba6618c579fc20bb5d2cb8ae2efd21
Cultz20 <cultzbtra@gmail.com>
2025-03-31 10:08:47 +0700

    Add files via upload

---
+++ 6.1/LookCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations
+{
+    public class LookCommand : Command
+    {
+        public LookCommand() : base(new string[] {"look"})
+        {
+
+        }
+        public override string Execute(Player p, string[] text)
+        {
+            if (text.Length == 0 || text[0] != "look")
+                return "Error in look input";
+            if (text.Length != 3 && text.Length != 5)
+                return "I don't know how to look like that";
+            if (text[1] != "at")
+                return "What do you want to look at?";
+            if (text.Length == 5 && text[3] != "in")
+                return "What do you want to look in?";
+
+            IHaveInventory container = (text.Length == 3) ? p : FetchContainer(p, text[4]);
+
+            if (container == null)
+                return $"I cannot find the {text[4]}";
+
+            return LookAtIn(text[2], container);
+        }
+
+        private IHaveInventory? FetchContainer(Player p, string containerId)
+        {
+            IHaveInventory? container = p.Locate(containerId) as IHaveInventory;
+            if (container == null)
+                return null;
+
+            return container;
+        }
+
+        private string LookAtIn(string thingId, IHaveInventory container)
+        {
+            if (container == null)
+                return "Error: Container is null"; // Safety check
+
+            GameObject item = container.Locate(thingId);
+            if (item == null)
+                if (container is Player)
+                    return $"I cannot find the {thingId}";
+                else
+                    return $"I cannot find the {thingId} in the {container.Name}";
+
+            return item.ToString();
+        }
+
+    }
+}
---
+++ IterationsTest/PlayerTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Iterations;
+using NUnit.Framework;
+
+
+namespace TestIteration
+{
+    [TestFixture]
+    public class PlayerTest
+    {
+        private Item bronze_sword = new Item(new string[] { "sword" }, "bronze sword", "a bronze sword");
+        private Item bronze_shovel = new Item(new string[] { "shovel" }, "bronze shovel", "a bronze shovel");
+        private Player player = new Player("Me", "an adventurer");
+
+        [SetUp]
+        public void Setup()
+        {
+            player.Inventory.Put(bronze_sword);
+            player.Inventory.Put(bronze_shovel);
+        }
+        [Test]
+        public void TestPlayerIsIdentifiable()
+        {
+            Assert.IsTrue(player.AreYou("me"));
+            Assert.IsTrue(player.AreYou("inventory"));
+        }
+        [Test]
+        public void TestPlayerLocatesItems()
+        {
+            Assert.That(player.Locate("sword"), Is.EqualTo(bronze_sword));
+            Assert.That(player.Locate("shovel"), Is.EqualTo(bronze_shovel));
+            Assert.IsTrue(player.Inventory.HasItem("sword"));
+            Assert.IsTrue(player.Inventory.HasItem("shovel"));
+        }
+        [Test]
+        public void TestPlayerLocatesItSelf() 
+        {
+            Assert.That(player.Locate("me"), Is.EqualTo(player));
+            Assert.That(player.Locate("inventory"), Is.EqualTo(player));
+        }
+        [Test]
+        public void TestPlayerLocatesNothing()
+        {
+            Assert.IsNull(player.Locate("helmet"));
+            Assert.IsNull(player.Locate("chestplate"));
+        }
+        [Test]
+        public void TestPlayerFullDescription()
+        {
+            string expected = "Me, you are an adventurer. You are carrying: bronze sword, bronze shovel";
+            Assert.That(player.FullDescription, Is.EqualTo(expected));
+        }
+    }
+}
