91f36d60e8aa82738cf3b5cabbf2d9094a0ae1c7
Cultz20 <cultzbtra@gmail.com>
2025-03-31 10:28:09 +0700

    Add files via upload

---
+++ Week 6/6.1/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations
+{
+    public class Player : GameObject, IHaveInventory
+    {
+        Inventory _inventory;
+
+        public Player(string name, string desc) :base(new string[] {"me", "inventory"}, name, desc)
+        {
+            Name = name;
+            Description = desc;
+            _inventory = new Inventory();
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public GameObject Locate(string id)
+        {
+            if (AreYou(id))
+            {
+                return this;
+            }
+            else return _inventory.Fetch(id);
+        }
+
+        public override string FullDescription
+        {
+            get
+            {
+                return $"{Name}, you are {Description}. You are carrying: {_inventory.ItemList}";
+            }
+        }
+        public Inventory Inventory
+        {
+            get { return _inventory; }
+
+        }
+    }
+}
